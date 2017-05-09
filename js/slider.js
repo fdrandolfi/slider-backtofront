6e204434befbded07058187b3eab7b214cbfaac0
Felipe Randolfi <fdrandolfi@gmail.com>
2017-05-09 16:28:13 -0400

    Actualizando CSS y corriengo JS

--- js/slider.js
+++ js/slider.js
@@ -4,52 +4,57 @@
 
 var slider = $('.sh_cont');
 var link = $('.sh_cont .sh_slide a');
-var titulo = $('.sh_cont .sh_slide h1');
-var titulo2 = $('.sh_cont .sh_slide h2');
+var h1 = $('.sh_cont .sh_slide h1');
+var h2 = $('.sh_cont .sh_slide h2');
 var img = $('.sh_cont .sh_slide img');
 
 
 	// Init
-	link.fadeIn(750);
-	titulo.fadeIn(750);
-	titulo2.fadeIn(750);
 	window.setTimeout(function(){
-			img.fadeIn(750);
+			link.fadeIn(1000);
+			h1.fadeIn(1200);
+			h2.fadeIn(1000);
 		}, 500);
 	window.setTimeout(function(){
-			link.fadeOut(750);
-			titulo.fadeOut(750);
-			titulo2.fadeOut(750);
+			img.fadeIn(1000);
+		}, 500);
+	window.setTimeout(function(){
+			link.fadeOut(1000);
+			h1.fadeOut(1200);
+			h2.fadeOut(1000);
 		}, 6000);
 	window.setTimeout(function(){
-			img.fadeOut(750);
+			img.fadeOut(1000);
 		}, 6500);
 
 function mover(){
 
-	link.fadeIn(750);
-	titulo.fadeIn(750);
-	titulo2.fadeIn(750);
+	// Settings Moves
 	window.setTimeout(function(){
-			img.fadeIn(750);
+			link.fadeIn(1000);
+			h1.fadeIn(1200);
+			h2.fadeIn(1000);
 		}, 500);
 	window.setTimeout(function(){
-			link.fadeOut(750);
-			titulo.fadeOut(750);
-			titulo2.fadeOut(750);
+			img.fadeIn(1000);
+		}, 500);
+	window.setTimeout(function(){
+			link.fadeOut(1000);
+			h1.fadeOut(1200);
+			h2.fadeOut(1000);
 		}, 6000);
 	window.setTimeout(function(){
-			img.fadeOut(750);
+			img.fadeOut(1000);
 		}, 6500);
 
 	// Infinite Cicle
-		$('.sh_cont .sh_slide:first').insertAfter('.sh_cont .sh_slide:last');
+	$('.sh_cont .sh_slide:first').insertAfter('.sh_cont .sh_slide:last');
 }
 
 function autoplay(){
 	interval = setInterval(function(){
 		mover();
-	}, 9250);
+	}, 8000);
 }
 
 autoplay();
